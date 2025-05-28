4d9a359f105a48555041fe860b7c0afe1ddfbfe0
Yuta998 <yutatiba32@gmail.com>
2025-05-28 11:37:20 +0000

    test中

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 'use strict';
 
+const fs = require('fs');
+const readline = require('readline');
+const rs =fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({'input':rs,'output':{}});
 
-aaaaaaaaaaaaaaaa
+
+rl.on('line',(lineString) => {
+		console.log(lineString);
+});
+
+rl.resume();
+
+
+
 
 
